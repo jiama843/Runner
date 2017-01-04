a931d96e6128f7fd3b6eed9b9a9b4c59e7a682d5
John <John@v1022-rn-136-226.campus-dynamic.uwaterloo.ca>
2017-01-04 01:06:56 -0500

    first commit

---
+++ app/src/main/java/com/ma/john/weebrunner/End_Activity.java
@@ -0,0 +1,13 @@
+package com.ma.john.weebrunner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class End_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end_);
+    }
+}
